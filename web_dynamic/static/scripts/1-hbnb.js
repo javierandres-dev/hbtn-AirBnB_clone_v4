b8d05645ffe30c697bd88d245f2953d0147e08c7
0x00developer <info@tecnoayuda.co>
2020-05-15 18:32:17 -0500

    [Feature] Task 1. 	modified:   static/scripts/1-hbnb.js

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,9 +1,20 @@
-if (typeof jQuery === 'function') {
-  $(document).ready(function () {
-    let listAmenityId = [];
-    function recordCheck(){
-      console.log('Works!');
+$( document ).ready(function() {
+  const checkbox = $('.amenities input[type="checkbox"]');
+  checkbox.bind('click',function () {
+    const id = $(this).attr('data-id');
+    const name = $(this).attr('data-name');
+    const obj = {};
+    const arr = [];
+    if (this.checked) {
+      obj[id] = name;
+    } else {
+      delete (obj[id]);
     }
-    $('.amenities ul li input[type=checkbox]').bind('click',recordCheck);
+    for (const i in obj) {
+      arr.push(obj[i]);
+    }
+    let names = arr.join(', ');
+    $('.amenities h4').text(names);
+    console.log(arr);
   });
-}
+});
